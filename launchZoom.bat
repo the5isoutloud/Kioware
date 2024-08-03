74080612fa1d7a4dc647be780f14d776c4fef376
the5isoutloud <past_pension_0n@icloud.com>
2024-08-03 12:57:11 -0700

    purge

--- launchZoom.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" "C:\Users\User\AppData\Roaming\Zoom\bin\Zoom.exe" --url="zoommtg://zoom.us/join?action=join&confno=8016354627"
--- old/Kioware-node/launch.js
+++
@@ -1,8 +0,0 @@
-// launch.js
-
-const app = require('./app');
-
-const port = 3333;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
-});
