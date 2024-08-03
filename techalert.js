38fd8753994dc53655d3098539dc7d6601a25001
the5isoutloud <past_pension_0n@icloud.com>
2024-08-03 09:46:31 -0700

    first commit

---
+++ techalert.js
@@ -0,0 +1,7 @@
+document.addEventListener('keydown', function(event) {
+    // Check if the F9 key was pressed
+    if (event.key === 'F9') {
+        // Open the Zoom meeting URL
+        window.open('https://zoom.us/j/8016354627', '_blank');
+    }
+});
